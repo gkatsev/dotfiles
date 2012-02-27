309fb9259b1161808ed513667090ea9d3a92031f
Gary Katsevman <git@gkatsev.com>
2012-02-27 02:22:02 -0500

    Add install script.

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+VIMRC="ln -s `pwd`/vimrc .vimrc"
+VIMDIR="ln -s `pwd`/vim .vim"
+VIMPRC="ln -s `pwd`/vimperatorrc .vimperatorrc"
+VIMPDIR="ln -s `pwd`/vimperator .vimperator"
+GITRC="ln -s `pwd`/gitconfig .gitconfig"
+HGRC="ln -s `pwd`/hgrc .hgrc"
+TTY="ln -s `pwd`/ttytterrc .ttytterrc"
+
+cd
+
+echo "Linking .vimrc"
+echo $VIMRC
+$VIMRC
+echo "Linking .vim"
+echo $VIMDIR
+$VIMDIR
+
+echo "Linking .vimperatorrc"
+echo $VIMPRC
+$VIMPRC
+echo "Linking .vimperator"
+echo $VIMPDIR
+$VIMPDIR
+
+echo "Linking .gitconfig"
+echo $GITRC
+$GITRC
+echo "Linking .hgrc"
+echo $HGRC
+$HGRC
+
+echo "Linking .ttytterrc"
+echo $TTY
+$TTY
+
+echo "Finished installing"
