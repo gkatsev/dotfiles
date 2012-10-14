de3d5aa1d83fe1b073dfe406faf1c43b99a7243c
Gary Katsevman <git@gkatsev.com>
2012-10-14 00:35:36 -0400

    apply cmr's patches

--- install.sh
+++ install.sh
@@ -1,43 +1,11 @@
 #!/usr/bin/env sh
 
-VIMRC="ln -s `pwd`/vimrc .vimrc"
-VIMDIR="ln -s `pwd`/vim .vim"
-VIMPRC="ln -s `pwd`/vimperatorrc .vimperatorrc"
-VIMPDIR="ln -s `pwd`/vimperator .vimperator"
-GITRC="ln -s `pwd`/gitconfig .gitconfig"
-HGRC="ln -s `pwd`/hgrc .hgrc"
-TTY="ln -s `pwd`/ttytterrc .ttytterrc"
-SCREEN="ln -s `pwd`/screenrc .screenrc"
-
-cd
-
-echo "Linking .vimrc"
-echo $VIMRC
-$VIMRC
-echo "Linking .vim"
-echo $VIMDIR
-$VIMDIR
-
-echo "Linking .vimperatorrc"
-echo $VIMPRC
-$VIMPRC
-echo "Linking .vimperator"
-echo $VIMPDIR
-$VIMPDIR
-
-echo "Linking .gitconfig"
-echo $GITRC
-$GITRC
-echo "Linking .hgrc"
-echo $HGRC
-$HGRC
-
-echo "Linking .ttytterrc"
-echo $TTY
-$TTY
-
-echo "Linking .screenrc"
-echo $SCREEM
-$SCREEN
+files="vimrc vimperatorrc vimperator gitconfig hgrc ttytterrc screenrc"
+for file in $files; do
+  echo "Linking .$file"
+  cmd="ln -s `pwd`/$file $HOME/.$file"
+  echo $cmd
+  eval $cmd
+done
 
 echo "Finished installing"
